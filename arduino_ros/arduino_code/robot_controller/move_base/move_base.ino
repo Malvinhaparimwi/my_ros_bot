8c0e417f116a5e4a009e841dc3a3cdf34aba12d7
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2026-03-05 17:36:59 +0200

    Custom Controller node working

--- arduino_ros/arduino_code/robot_controller/move_base/move_base.ino
+++ arduino_ros/arduino_code/robot_controller/move_base/move_base.ino
@@ -1,11 +1,13 @@
+#include <ArduinoJson.h>
+
 // Motors Pins
 const int in1 = 7;
 const int in2 = 8;
-const int in3 = 11;
+const int in3 = 12;
 const int in4 = 10;
 
 const int ena = 9;
-const int enb = 12;
+const int enb = 11;
 
 void setup(){
   // Motors Pins Configuration
@@ -20,31 +22,66 @@ void setup(){
 }
 
 void loop(){
-  if(Serial.available()){
-    char command = Serial.read();
+    if (Serial.available() > 0) {
+      // Read the JSON string
+      String input = Serial.readStringUntil('\n');
+  
+      // Create a buffer for the JSON object
+      StaticJsonDocument<200> doc;
+      DeserializationError error = deserializeJson(doc, input);
+  
+      if (!error) {
+        // ACCESS INDIVIDUALLY HERE
+        float lx = doc["linear_x"];
+        float az = doc["angular_z"];
 
-    if(command == '1'){
-      driveRobot();
-    } else if(command == '0') {
-      stopRobot();
+        driveRobot(lx, az);
+      } 
     }
+}
+
+void driveRobot(float lx, float az) {
+
+  // Stop Condition
+  if (abs(lx) < 0.01 && abs(az) < 0.01) {
+    digitalWrite(in1, LOW); digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW); digitalWrite(in4, LOW);
+    analogWrite(ena, 0);
+    analogWrite(enb, 0);
+    return;
   }
-}
 
-void driveRobot(){
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(ena,200);
-  
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  analogWrite(enb,200);
-}
+  // Map velocities to PWM while keeping sign
+  float linear_pwm  = (lx / 0.22) * 200.0;
+  float angular_pwm = (az / 0.22) * 200.0;
 
-void stopRobot(){
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
+  // Differential drive
+  float leftSpeed  = linear_pwm + angular_pwm;
+  float rightSpeed = linear_pwm - angular_pwm;
+
+  // Limit speeds
+  leftSpeed  = constrain(leftSpeed,  -200, 200);
+  rightSpeed = constrain(rightSpeed, -200, 200);
+
+  // Left motor direction
+  if (leftSpeed >= 0) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  } else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+
+  // Right motor direction
+  if (rightSpeed >= 0) {
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+  } else {
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+  }
+
+  // Send PWM
+  analogWrite(ena, abs(leftSpeed));
+  analogWrite(enb, abs(rightSpeed));
 }
