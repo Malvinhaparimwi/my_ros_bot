90a53590f72cd4d85c0e8cdf077c7a69ae08891d
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2026-04-01 22:36:34 +0000

    everything now being started with a launch file

---
+++ src/onyx_robot/launch/robot_launch.py
@@ -0,0 +1,35 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='arduino_ros',
+            executable='controller',
+            name='motor_controller',
+            output='screen'
+            ),
+
+        Node(
+            package='ros_bot_imu',
+            executable='imu_begin',
+            name='imu_node',
+            output='screen'
+            ),
+
+        Node(
+            package='camera_ros',
+            executable='camera_node',
+            name='camera_node',
+            output='screen',
+            parameters=[
+                {
+                    'width': 640,
+                    'height': 480,
+                    'frame_rate': 30.0,
+                    'format': 'BGR888',
+                    'auto_exposure': 0,
+                    'exposure_time': 15000,
+                    'buffer_count': 4}])
+        ])
